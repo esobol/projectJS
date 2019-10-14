7533c11380541fc491f47d0489b34524b17a7feb
Elina <elina.sobol@gmail.com>
2019-10-14 14:05:02 -0700

    my first

---
+++ index.js
@@ -0,0 +1,50 @@
+'use strict';
+// alert("this is our first JS file");
+
+// var message;
+// message = "Hi";
+// message = 100;
+// console.log(message);
+
+// var sum = 1 + 1;
+// console.log(sum);
+// let box = document.getElementById('box');
+// console.log(box);
+
+
+var leffBorderWidght = 1;
+var string = "hello";
+var sym = Symbol();
+null;
+undefined;
+let second =2;
+const pi =3.14;
+
+console.log(second); // can't be 
+
+let persone = {
+    name: "Lina",
+    age: 28,
+    isMarried: false
+}
+
+console.log(persone.age);
+let array = ['plum.phg', 'apple.jpg', 'apple.bnp']
+console.log(array[1]);
+
+//alert('test123');
+// let answer = confirm('are you here');
+
+// console.log(answer);
+let answer = prompt('are you 18', 'yes');
+
+console.log(typeof (null));
+console.log(4 + "jh");
+
+
+
+
+
+
+
+
